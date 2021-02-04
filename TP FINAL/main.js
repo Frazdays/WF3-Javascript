9f339f372202de9bb8c66eba87faa935ba86fa2c
sophia <lazreg.sophia@gmail.com>
2021-02-04 14:20:08 +0100

    add tp

--- TP FINAL/main.js
+++ TP FINAL/main.js
@@ -8,20 +8,22 @@ for (let i = 0 ; i<= 52 ; i++){
     
     var createImg = document.createElement('img');
     createDiv.appendChild(createImg);
-    createImg.setAttribute('src' , 'https://via.placeholder.com/150x150/?text=Mistery+Card' );
+    createImg.setAttribute('src' , 'https://via.placeholder.com/150x150/'+'?text=Mistery+Card' );
     
-    createImg.addEventListener ("click",function (event) {
-        console.log(event);
-    });
-    createImg.setAttribute('src' , 'https://via.placeholder.com/150x150/'+'ff00ff/?text=Dame' );
-    
-
-
+    createImg.onclick= function (e) {
+        var letters = "0123456789ABCDEF";
+        var colors = "";
+        var nomCarte= ["roi","dame","as","valet"];
+        var text ="";
     
 
-
-       
-
+        for (let x = 0; x <= 6; x++) {
+            colors+= letters[Math.floor(Math.random()*16)];            
+        }
+        text+= nomCarte[Math.floor(Math.random()*3)];
+        let click =document.getElementById(i);
+        click.setAttribute ("src" , "https://via.placeholder.com/150x150/"+color+"?text="+text);
+    }
     
 }
 
